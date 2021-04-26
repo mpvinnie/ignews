3367d38b5ad3c69c481ff99929044110920677db
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-04-26 04:14:55 -0400

    Styling posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -10,22 +10,22 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
+          <a href="#">
             <time>29 de abril de 2021</time>
             <strong>Creating a Monorepo with Lerna and Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
           </a>
-          <a>
+          <a href="#">
             <time>29 de abril de 2021</time>
             <strong>Creating a Monorepo with Lerna and Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
           </a>
-          <a>
+          <a href="#">
             <time>29 de abril de 2021</time>
             <strong>Creating a Monorepo with Lerna and Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
           </a>
-          <a>
+          <a href="#">
             <time>29 de abril de 2021</time>
             <strong>Creating a Monorepo with Lerna and Yarn Workspaces</strong>
             <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
